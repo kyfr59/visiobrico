9cfab9fe47682cfe0d17d79314509c1218fad0f2
dupont-ext <kyfr59@gmail.com>
2025-12-29 14:54:21 +0100

    Remplacement de la page d'accueil par défaut et installation de Vite

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>VisioBrico</title>
+
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+<body>
+    <header>
+        <h1>Bienvenue sur VisioBrico</h1>
+    </header>
+    @yield('content')
+</body>
+</html>
